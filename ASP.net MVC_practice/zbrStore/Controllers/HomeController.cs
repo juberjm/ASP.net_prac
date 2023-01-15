02b4d9392e5686914b363be7cb93ab5e39a84855
juberjm <juberjm1258@gmail.com>
2023-01-15 14:29:08 +0530

    dotnet MVC model practice

---
+++ ASP.net MVC_practice/zbrStore/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using zbrStore.Models;
+
+namespace zbrStore.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+     public IActionResult Login()
+    {
+        return View();
+    }
+
+
+
+
+    public IActionResult validate(string email, string password)
+    {
+        if(email=="zbrstore@gmail.com" && password=="zbr"){
+            return Redirect("/Home/welcome");
+        }
+        return View();
+    }
+
+     public IActionResult welcome()
+    {
+        return View();
+    }
+
+     public IActionResult Register()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
